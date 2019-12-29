be32d2fc5253f3f06bc1732774dc4b400bca79a9
Johann Prell <johann.prell@gmail.com>
2019-12-29 15:08:30 +0100

    * simplistic vr input

--- Assets/_Bumpy/Scripts/OVRInputActions.cs
+++ Assets/_Bumpy/Scripts/OVRInputActions.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class OVRInputActions : MonoBehaviour
 {
+    public Spawner spawner;
+    public Transform leftOrigin;
+    public TokenKey leftKey;
+    public Transform rightOrigin;
+    public TokenKey rightKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,23 @@ public class OVRInputActions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+        {
+            triggerLeft();
+        }
+        if(OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        {
+            triggerRight();
+        }
+    }
+
+    private void triggerLeft()
+    {
+        spawner.SpawnToken(leftOrigin, leftKey);
+    }
+
+    private void triggerRight()
+    {
+        spawner.SpawnToken(rightOrigin, rightKey);
     }
 }
